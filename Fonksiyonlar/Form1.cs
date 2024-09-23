7765b14f852451d9e6a7c0b26e6e32b052233113
ayhan67 <117070333+ayhan67@users.noreply.github.com>
2024-09-24 00:05:04 +0300

    Update Form1.cs

--- Fonksiyonlar/Form1.cs
+++ Fonksiyonlar/Form1.cs
@@ -6,18 +6,18 @@ namespace Fonksiyonlar
         {
             InitializeComponent();
         }
-        private int toplamaYapveGoster(int sayi1, int sayi2)
+        private int toplamaYapveGoster(int sayi4, int sayi2)
         {
-            int toplam = sayi1 + sayi2;
+            int toplam = sayi4 + sayi2;
             return toplam;
         }
 
         
         private void button1_Click(object sender, EventArgs e)
         {
-            int sayi1 = Convert.ToInt32(textBox1.Text);
+            int sayi4 = Convert.ToInt32(textBox1.Text);
             int sayi2 = Convert.ToInt32(textBox2.Text);
-            int top = toplamaYapveGoster(sayi1, sayi2);
+            int top = toplamaYapveGoster(sayi4, sayi2);
             label1.Text = top.ToString();
         }
     }
